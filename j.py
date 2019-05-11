c3172ca0521caa6071bb726b5a432d2f7290cec1
inyour77 <50392504+inyour77@users.noreply.github.com>
2019-05-11 16:32:54 +0700

    Add files via upload

---
+++ j.py
@@ -0,0 +1,121 @@
+#Compiled InYoyurXerXez7
+#2e4hTeam
+#Kiya
+import os
+import sys
+import time
+from time import sleep
+
+g = "\033[32;1m"
+gt = "\033[0;32m"
+bt = "\033[34;1m"
+b = "\033[36;1m"
+m = "\033[31;1m"
+c = "\033[0m"
+p = "\033[37;1m"
+u = "\033[35;1m"
+M = "\033[3;1m"
+k = "\033[33;1m"
+kt = "\033[0;33m"
+a = "\033[30;1m"
+
+W = "\x1b[0m"
+R = "\x1b[31m"
+G = "\x1b[1;32m"
+O = "\x1b[33m"
+B = "\x1b[34m"
+P = "\x1b[35m"
+C = "\x1b[36m"
+GR = "\x1b[37m"
+def slowprints(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(2.0/90)
+
+os.system("clear")
+gilang=(a+"""
+==============<·~~~~~~~~~~~~>·==============
+| Author : InYourXerXez7                 |
+| Team   : Buft~2e4h GOBLOG CREW         |
+| ThankTo: My Friends && Allah           |
+| Codex  :https://github.com/gillanggans7|
+| Contack: @XerXezOficial                |
+==============<·~~~~~~~~~~~~>·==============
+""")
+gil=(a+"""
+<|>\\\\\\\\\\\\\\\\|//////////////////<|>
+\ \x1b[35m AKU BUKAN BADUT PENGHIBUR  \033[30;1m/
+/ \x1b[35m DISAAT PANGERAN MENGHILANG \033[30;1m\
+\ \x1b[35m ~TAKUR \033[30;1m
+<|>\\\\\\\\\\\\\\\\|//////////////////<|>
+""")
+tol=(c+"""
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+| \x1b[35m Lakukanlah apa saja yang membuat bahagia  \033[0;1m|
+| \x1b[35m tapi inget semua ada pertanggung jawabnnya\033[0;1m|
+| \x1b[35m dan semoga pembalasan cepet datangnya! :')\033[0;1m|
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+""")
+
+
+lol=(c+"""
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+| \x1b[35m Jangan Bersedih, Kamu bisa    \033[0;1m|
+| \x1b[35m Meluk dirimu Sendiri :) Takur!\033[0;1m|
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+""")
+sat=(c+"""
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+| \x1b[35m Jika hanya sebatas janji bisa membuat tenang \033[0;1m|
+| \x1b[35mlantas mau sampai kapan berbohong membuat bahagia?!! \033[0;1m|
+| \x1b[35m ~TaKuR                           :') \033[0;1m|
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+""")
+bang=(c+"""
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+| \x1b[35m Dari pada menjadi teman yang palsu \033[0;1m|
+| \x1b[35m lebih baik menjadi musuh yang nyata!\033[0;1m|
+| \x1b[35m ~TaKuR                           :')\033[0;1m|
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+""")
+jing=(c+"""
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+| \x1b[35m Bisakah kamu merindukanku kembali?\033[0;1m|
+| \x1b[35m Aku Tidak kuat merindu sendirian. \033[0;1m|
+| \x1b[35m ~TaKuR                           :')\033[0;1m|
+{|}~~\~~~|~~~\~~~\~~~\~~~\~~~\~~~\~~~\~~~|~~~{|}
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+os.system("pkg update && upgrade")
+os.system("pkg install ruby")
+os.system("gem install lolcat")
+sleep(4)
+os.system("clear")
+print(gilang)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+print(gil)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+slowprints(tol)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+slowprints(lol)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+slowprints(sat)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+slowprints(bang)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+slowprints(jing)
+input('\n \033[1;31mEnTerUnTukLanjut~$ ')
+sleep(20)
+os.system("exit")
